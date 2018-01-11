da7eb98306bcbbb0a2d0eb47dfbcedee672caf68
Amy Lumpkin <amylumpkin@Amys-MacBook-Pro.local>
2018-01-10 21:29:58 -0500

    anything

--- assets/app.js
+++ assets/app.js
@@ -1,5 +1,7 @@
 const apikey = "f6476d7762e54a2c8da7593eb46dec0c";
 
+const articles;
+
 function getData(q,limit,start,end) {
 	const url="http://api.nytimes.com/svc/search/v2/articlesearch.json?api-key=" + apikey + "&q=" + q + "&limit=" + limit;
 	if (start !== undefined)
@@ -9,12 +11,31 @@ function getData(q,limit,start,end) {
 
 	$.ajax({
 		method: "GET", 
-		url: " "
+		url: url
 	})
 	.done(function(data) {
-		return data;
+		articles = data.response.docs;
+
 	})
 }
+
+function renderArticles(articles){
+	for (let i = 0; i < articles.length; ++i){
+		//headline -- .headline.print_headline
+		//author -- .byline.original
+		//snippet -- .snippet
+		//url -- web_url
+	}
+}
+function createArticle(headline,author,snippet,url){
+	let newArticle=$("<div>");
+	let header=$("<a>").attr("href", url);
+	$("<h1>").text(headline).appendTo(header);
+	header.appendTo(newArticle);
+	$("<p>").text(snippet).appendTo(newArticle);
+	//here we append our new article to the div where articles go
+}
+
 $(document).ready(function(){
 	$("body").empty();
 	$("body").text(getData("jack kirby", 5));
