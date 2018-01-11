dfa24bd0229451799a9048ab741d7b90fd779690
Amy Lumpkin <amylumpkin@Amys-MacBook-Pro.local>
2018-01-10 21:04:28 -0500

    test

--- assets/app.js
+++ assets/app.js
@@ -1,7 +1,7 @@
 const apikey = "f6476d7762e54a2c8da7593eb46dec0c";
 
 function getData(q,limit,start,end) {
-	const url="https://api.nytimes.com/svc/search/v2/articlesearch.json?api-key=" + apikey + "&q=" + q + "&limit=" + limit;
+	const url="http://api.nytimes.com/svc/search/v2/articlesearch.json?api-key=" + apikey + "&q=" + q + "&limit=" + limit;
 	if (start !== undefined)
 		url += "&begin_date=" + start;
 	if (end !== undefined)
@@ -12,6 +12,10 @@ function getData(q,limit,start,end) {
 		url: " "
 	})
 	.done(function(data) {
-
+		return data;
 	})
 }
+$(document).ready(function(){
+	$("body").empty();
+	$("body").text(getData("jack kirby", 5));
+});
